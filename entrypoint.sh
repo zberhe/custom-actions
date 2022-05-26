fc82744e85f5c85b36ff9169c43101a525d2d477
Zehafta Berhe <zehaftatekea@gmail.com>
2022-05-26 16:29:28 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 api_url="https://api.agify.io/?name=${INPUT_NAME}"
 echo $api_url
 
-person_name=$(curl "${api_url}" | jq ".name")
-echo $person_name
+name=$(curl "${api_url}" | jq ".name")
+echo $name
 
-echo "::set-output name=person_name::$person_name"
+echo "::set-output name=person_name::$name"
